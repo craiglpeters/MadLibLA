9f7bc9f1cb9c7544440555c83e55d1950cf544a8
Craig Peters <craig.l.peters@gmail.com>
2021-02-08 15:43:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Mad Lib inspired story
+# Author: Craig Peters
+
+print("Walking aroud LA: inspired by Mad Lib.\n" +
+"You are about to play a silly word game.\n" +
+"I will ask you for nouns, verbs, adjectives, proper nouns and adverbs.\n" +
+"Using those words, the program will create a hopefully surprising story " +
+"for you.\n\nProvide...")
+
+yourname = input("Your name: ")
+propernoun1 = input("Proper noun - a well known person: ")
+adverb1 = input("Adverb: ")
+
+print("\n\nWalking around LA\n\tby " + yourname + "\n\nAs I meandered down" +
+   " Hollywood Blvd, just after I stepped over " + propernoun1 + "'s star, " +
+   "Kermit the Frog popped out of a doorway with a shocking proposition. In a " +
+   adverb1 + " high pitched voice he told me we should partner on a new project.")
